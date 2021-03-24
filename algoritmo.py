d4d583fbe182648b08e7870ae11924d7b12368c7
Loddrik <ruben.hermosilla@mail.udp.cl>
2021-03-23 23:50:20 -0300

    funciona gentee

--- algoritmo.py
+++ algoritmo.py
@@ -29,8 +29,8 @@ def irr(datos):
     irr = np.irr(datos)
     return irr
 
-def cae(irr,meses):
-    return irr*meses*100
+def cae(irr):
+    return irr*12*100
 
 def ajustar_interes(interes):
     interes = interes/100
@@ -62,7 +62,7 @@ def Algoritmo(capital,i,meses,gastosAsociados,seguro):
     #calculamos irr
     tir = irr(datos)
     #calculamos cae
-    CAE = "{0:.2f}".format(cae(tir,meses)) #Nose si redondear o truncar round(cae(tir,meses),2)
+    CAE = "{0:.2f}".format(cae(tir)) #Nose si redondear o truncar round(cae(tir,meses),2)
     #calculamos intereses acumulados
     acumulados = interes_acumulado(total,meses,i,cuota)
     #calculamos costo total del credito
@@ -73,16 +73,30 @@ def Algoritmo(capital,i,meses,gastosAsociados,seguro):
 
     
 
-# if __name__== "__main__":
+if __name__== "__main__":
+    #[nombre,monto, interes,meses,gastos_asociados,total_seguro]
+    s1 = ['Banco Consorcio',1000000,1.18,12,1000,12324]
+    s2 = ['Banco de Chile',1000000,2.26,12,9692,7345]
+    s3 = ['Banco Estado',3000000,1.55,24,25219,39697]
+    s4 = ['Banco Santander',3000000,2.03,24,27746,65492]
+    s5 = ['Banco Estado',5000000,1.25,36,41562,66155]
+    s6 = ['Scotiabank Azul',5000000,2.16,36,43603,126640]
+    s7 = ['Caja los Andes',7000000,1.10,48,58184,127371]
+    s8 = ['Banco Security',7000000,1.58,48,60618,329171]
+    s9 = ['Banco de Chile',9000000,1.6,60,76792,327735]
+    s10 = ['Caja la Araucana',10000000,1.62,60,81451,1294440]
 
-#     tir,CAE,acumulados,total,costo_total,cuota = ALgoritmo(1000000,1.99,12,8839,8589)
+    simulaciones = [s1,s2,s3,s4,s5,s6,s7,s8,s9,s10]
 
-#     print('tir : ' + str(tir))
-#     print(str(CAE))
-#     print(str(int(round(acumulados))))
-#     print(str(int(round(total))))
-#     print(str(int(round(costo_total))))
-#     print(str(int(round(cuota))))
+    for s in simulaciones:
+        tir,CAE,acumulados,total,costo_total,cuota = Algoritmo(s[1],s[2],s[3],s[4],s[5])
+        print(s[0])
+        print('CAE ' + str(CAE) + '% ' + 'Cuota ' + str(costo_total))
+        print('\n')
+
+    
+
+    
 
 
 
